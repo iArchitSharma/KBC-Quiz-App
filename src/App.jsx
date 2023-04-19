58d40cfd7d4c6fcefdfbc8233b49d5ed1c973b68
Archit Sharma <archit8679@gmail.com>
2023-04-19 18:45:07 +0530

    Hooks added for Active class

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,46 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [questionNumber, setQuestionNumber] = useState(1);
 
+  const moneyTable = [
+    {id:1, amount:"$ 100"},
+    {id:2, amount:"$ 200"},
+    {id:3, amount:"$ 300"},
+    {id:4, amount:"$ 400"},
+    {id:5, amount:"$ 500"},
+    {id:6, amount:"$ 600"},
+    {id:7, amount:"$ 700"},
+    {id:8, amount:"$ 800"},
+    {id:9, amount:"$ 900"},
+    {id:10, amount:"$ 1000"},
+    {id:11, amount:"$ 1500"},
+    {id:12, amount:"$ 2500"},
+    {id:13, amount:"$ 5000"},
+    {id:14, amount:"$ 1000"}
+  ].reverse();
   return (
     <div className="app">
-      <div className="main">main</div>
+      <div className="main">
+        <div className="top">
+          <div className="timer">30</div>
+        </div>
+        <div className="bottom">Question/Answers</div>
+      </div>
+
       <div className="moneyTable">
-      <ul className='moneyList'>
-        <li className='moneyListItem'>
-          <span className="moneyListItemNumber"></span>
-        </li>
-        <li className='moneyListItem'>$ 300</li>
-        <li className='moneyListItem'>$ 200</li>
-        <li className='moneyListItem'>$ 100</li>
-      </ul>
+        <ul className="moneyList">
+          {moneyTable.map((i) => (
+            //Active class using hooks
+            <li className = {questionNumber === i.id ? "moneyListItem active" : "moneyListItem"}>
+              <span className="moneyListItemNumber">{i.id}</span>
+              <span className="moneyListItemAmount">{i.amount}</span>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
-  )
+  );
 }
 
 export default App
